3049541b1d9835fa8f64b2a8b54bc95f4f579385
Seb Martin <sebastien.martin@shopify.com>
2018-07-02 00:16:08 -0400

    first commit -- simple menubar

---
+++ Sources/ghn/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  ghn
+//
+//  Created by Sebastien Martin on 2018-07-01.
+//  Copyright © 2018 Seb Martin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		if let button = statusItem.button {
+			button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+			button.action = #selector(printQuote(_:))
+		}
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+	@objc func printQuote(_ sender: Any?) {
+		let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+		let quoteAuthor = "Mark Twain"
+
+		print("\(quoteText) — \(quoteAuthor)")
+	}
+
+}
+
