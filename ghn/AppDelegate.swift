a36c5020525d33f10b3bf0b98e1a4eb38a8cac89
Seb Martin <sebastien.martin@shopify.com>
2018-07-01 23:26:47 -0400

    Initial Commit

---
+++ ghn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ghn
+//
+//  Created by Sebastien Martin on 2018-07-01.
+//  Copyright © 2018 Seb Martin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ ghn/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ghn
+//
+//  Created by Sebastien Martin on 2018-07-01.
+//  Copyright © 2018 Seb Martin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
